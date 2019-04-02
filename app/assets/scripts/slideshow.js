fde0a37e9fe2a4496dfdba37e3f013904c17fa63
Moltus <moltus@protonmail.com>
2019-04-02 21:06:06 +0200

    outline for slideshow, tweaks for catchphrase lines

--- app/assets/scripts/slideshow.js
+++ app/assets/scripts/slideshow.js
@@ -27,3 +27,17 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
+
+function autoSlides() {
+  let i;
+  let slides = document.getElementsByClassName("slideshow__slide");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none"; 
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1} 
+  slides[slideIndex-1].style.display = "block"; 
+  setTimeout(autoSlides, 5000); // Change image every 2 seconds
+}
+
+autoSlides();
