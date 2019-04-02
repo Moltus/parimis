ff96395067865801d942c420216fc1f94975aa39
Moltus <moltus@protonmail.com>
2019-04-03 00:16:48 +0200

    went back to display block slider

--- app/assets/scripts/slideshow.js
+++ app/assets/scripts/slideshow.js
@@ -1,3 +1,6 @@
+let slides = document.getElementsByClassName("slideshow__slide");
+let dots = document.getElementsByClassName("slideshow__dot");
+
 let slideIndex = 1;
 showSlides(slideIndex);
 
@@ -13,31 +16,32 @@ function currentSlide(n) {
 
 function showSlides(n) {
   let i;
-  let slides = document.getElementsByClassName("slideshow__slide");
-  let dots = document.getElementsByClassName("slideshow__dot");
+  
   if (n > slides.length) slideIndex = 1;
   if (n < 1) slideIndex = slides.length;
   for (let s of slides) {
-    s.style.animation = "fade-out 1.5s";
+    s.style.display = "none";
   }
   for (let d of dots) {
     d.className = d.className.replace(" active", "");
   }
 
-  slides[slideIndex-1].style.animation = "fade-in 1.5s backwards";
+  slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
 
 function autoSlides() {
-  let i;
-  let slides = document.getElementsByClassName("slideshow__slide");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.animation = "fade-out 1.5s 1.5s forwards"; 
+  for (let s of slides) {
+    s.style.display = "none"; 
+  }
+  for (let d of dots) {
+    d.className = d.className.replace(" active", "");
   }
   slideIndex++;
   if (slideIndex > slides.length) {slideIndex = 1} 
-  slides[slideIndex-1].style.animation = "fade-in 1.5s forwards";
-  setTimeout(autoSlides, 5000); // Change image every 2 seconds
+  slides[slideIndex-1].style.display = "block"; 
+  dots[slideIndex-1].className += " active";
+  setTimeout(autoSlides, 6000); // Change image every 2 seconds
 }
 
 autoSlides();
